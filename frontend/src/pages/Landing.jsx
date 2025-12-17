3e4c14962422d91d1d12667bf62ee25049f9c1be
lordsainath <lordsainath8@gmail.com>
2025-12-17 10:41:50 +0530

    feat(ui): add responsive navbar for landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../components/navbar/Navbar'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <>
+      <Navbar/>
+    
+    </>
   )
 }
 
